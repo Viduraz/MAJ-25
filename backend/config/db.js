0b4a151a60bc97a6eb6de217a07d62b7f372b309
Viduraz <162951228+Viduraz@users.noreply.github.com>
2024-11-13 21:16:47 +0530

    done db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log(`successfully connected to mongoDB 😁`);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected heeeew gmmc whuttto: `);
     } catch (error) {
-        console.error(`ERROR: ${error.message}`);
+        console.error(`Error: ${error.message}`);
         process.exit(1);
     }
 };
