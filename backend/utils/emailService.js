a1c849ad0e85a2f922a9eb506f3e5b3276ac602b
Viduraz <162951228+Viduraz@users.noreply.github.com>
2024-11-18 20:27:04 +0530

    errors fixed

--- backend/utils/emailService.js
+++
@@ -1,6 +0,0 @@
-const nodemailer = require('nodemailer');
-
-const sendRegistrationEmail = async (registration) => {
-  // Configure email transport and send confirmation emails
-  // Implementation depends on your email service provider
-}; 
