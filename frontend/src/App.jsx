9eab537857f747f9ea236775cc7a6f2fa1251123
Viduraz <162951228+Viduraz@users.noreply.github.com>
2024-11-20 23:28:00 +0530

    started and done sign in

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Profile from './Pages/Profile'
+import Signin from './Pages/Signin'
+import Signup from './Pages/Signup'
+import Header from './Components/Header'
+
+export default function App () {
+  return <BrowserRouter>
+  {/** Header imported */}
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  
+  </BrowserRouter>
+}
