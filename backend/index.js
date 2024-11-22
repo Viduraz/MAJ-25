7d5ca3ca8eb2d9a0195fc0c57ba6a47433c5e36d
Viduraz <162951228+Viduraz@users.noreply.github.com>
2024-11-23 00:28:37 +0530

    db connected

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGOURI).then( () => {
+    console.log('DB connected Successfully');
+}).catch( (Err) => {
+    console.log(err);
+});
 
 const app = express();
 
