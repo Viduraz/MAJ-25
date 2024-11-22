fc04d7e46d81bda55ee4af42463774f368479894
Viduraz <162951228+Viduraz@users.noreply.github.com>
2024-11-23 01:04:26 +0530

    creating signup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.routes.js';
 
 dotenv.config();
 
@@ -15,3 +17,6 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server is running on port 4000');
     });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes); 
