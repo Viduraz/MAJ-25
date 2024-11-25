ce8b3c8b7dc06fd73ba6e34b2473157a5a314737
Viduraz <162951228+Viduraz@users.noreply.github.com>
2024-11-25 23:37:10 +0530

    firbase login done

--- frontend/src/Pages/Signin.jsx
+++ frontend/src/Pages/Signin.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { signInStart, signInSuccess, signInFailure } from '../redux/User/userSlice'
 import { useDispatch, useSelector } from 'react-redux';
-
+import OAuth from '../Components/OAuth';
 export default function Signin() {
   const [formData, setFormData] = useState({});
   const { loading, error } = useSelector((state) => state.user);
@@ -60,7 +60,7 @@ export default function Signin() {
         >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
-        
+        <OAuth />
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Dont Have an account?</p>
