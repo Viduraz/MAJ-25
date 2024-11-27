07432df6180c6fc95c01b8ded5c23f234ce959ac
Viduraz <162951228+Viduraz@users.noreply.github.com>
2024-11-27 23:36:28 +0530

    without photo uploading user updatation is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,8 @@ import Profile from './Pages/Profile'
 import Signin from './Pages/Signin'
 import Signup from './Pages/Signup'
 import Header from './Components/Header'
-
+import Registration from './Pages/Registration/Registration'
+import PrivateRoute from './Components/PrivateRoute'
 
 export default function App() {
   return <BrowserRouter>
@@ -14,10 +15,15 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route element={<PrivateRoute />} >
       <Route path="/profile" element={<Profile />} />
+      </Route>
       <Route path="/signin" element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
-    
+      <Route element={<PrivateRoute />} >
+      <Route path="/registration" element={<Registration />} />
+      </Route>
+     
     </Routes>
   
   </BrowserRouter>
