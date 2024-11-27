3b12d68ddab88df29ac772d3309c41144b48fe51
Viduraz <162951228+Viduraz@users.noreply.github.com>
2024-11-27 23:36:28 +0530

    without photo uploading user updatation is done

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { test, updateUser } from '../controllers/user.controller.js';
+import pkg from 'jsonwebtoken';      
+const { verify } = pkg;
+import { verifyToken } from '../Utils/VerifyUser.js';
 
 const router = express.Router();
 
 router.get('/', test);
+router.post("/update/:id", verifyToken, updateUser);
 
 export default router;
