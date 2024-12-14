6a49b2089b22344b57c5302b02bc896dfe8ccaf0
chathuwa-whiz <navodchathushka@gmail.com>
2024-12-15 00:23:58 +0530

    Add registration retrieval and toggle functionality in Signin and Sprofiles components

--- frontend/src/Pages/Signin.jsx
+++ frontend/src/Pages/Signin.jsx
@@ -3,13 +3,19 @@ import { Link, useNavigate } from 'react-router-dom';
 import { signInStart, signInSuccess, signInFailure } from '../redux/User/userSlice'
 import { useDispatch, useSelector } from 'react-redux';
 import OAuth from '../Components/OAuth';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import toast from 'react-hot-toast';
+import Switch from 'react-switch';
+import axios from 'axios';
 
 export default function Signin() {
   const [formData, setFormData] = useState({});
   const { loading, error } = useSelector((state) => state.user);
   const [message, setMessage] = useState('');
+  const [isSwitchOn, setIsSwitchOn] = useState(false);
+
+  const handleToggleSwitch = () => {
+    setIsSwitchOn(!isSwitchOn);
+  }
   
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -19,39 +25,69 @@ export default function Signin() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      dispatch(signInStart());
-      const res = await fetch('/api/auth/signin', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-      const data = await res.json();
-      
-      if (data.success === false) {
-        dispatch(signInFailure(data));
-        setMessage(data.message || 'Sign in failed.');
-        toast.error(data.message || 'Invalid credentials. Please try again.');
-        return;
+
+    if(isSwitchOn) {
+
+      // scout user registration
+      const URL = 'http://localhost:3000/api/registration/single';
+
+      try {
+        dispatch(signInStart());
+        const registration = await axios.post(URL, {
+          email: formData.email,
+          password: formData.password,
+        });
+        dispatch(signInSuccess(registration));
+        localStorage.setItem('registration', JSON.stringify(registration.data));
+        console.log('Registration:', registration.data);
+        
+        toast.success('Welcome back! You have successfully signed in.', setTimeout(2000));
+        navigate('/sprofiles');
+        
+      } catch (error) {
+        dispatch(signInFailure({ message: error.message }));
+        toast.error('Invalid credentials. Please try again.');
+        console.error('Error signing in:', error);
       }
-      dispatch(signInSuccess(data));
-      setMessage('Sign in successful!');
-      toast.success('Welcome back! You have successfully signed in.');
-      navigate('/');
-    } catch (error) {
-      dispatch(signInFailure({ message: error.message }));
-      setMessage('An error occurred. Please try again.');
-      toast.error('An error occurred. Please try again.');
+
+    } else {
+
+      // default user registration
+      try {
+        dispatch(signInStart());
+        const res = await fetch('/api/auth/signin', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        
+        if (data.success === false) {
+          dispatch(signInFailure(data));
+          setMessage(data.message || 'Sign in failed.');
+          toast.error(data.message || 'Invalid credentials. Please try again.');
+          return;
+        }
+        dispatch(signInSuccess(data));
+        setMessage('Sign in successful!');
+        toast.success('Welcome back! You have successfully signed in.');
+        navigate('/');
+      } catch (error) {
+        dispatch(signInFailure({ message: error.message }));
+        setMessage('An error occurred. Please try again.');
+        toast.error('An error occurred. Please try again.');
+      }
     }
   };
+
   return (
     <>
-      <ToastContainer />
       <div className='p-3 max-w-lg mx-auto'>
         <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
         <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+          
           <input
             type='email'
             placeholder='Email'
@@ -59,6 +95,7 @@ export default function Signin() {
             className='bg-slate-100 p-3 rounded-lg'
             onChange={handleChange}
           />
+
           <input
             type='password'
             placeholder='Password'
@@ -66,12 +103,23 @@ export default function Signin() {
             className='bg-slate-100 p-3 rounded-lg'
             onChange={handleChange}
           />
+
+          <div className="flex justify-end items-center mt-2">
+            <label htmlFor="scoutRegister" className="mr-4 text-gray-200">
+              Scout Register
+            </label>
+            <div className="relative">
+              <Switch onChange={handleToggleSwitch} checked={isSwitchOn} />
+            </div>
+          </div>
+
           <button
-            disabled={loading}
+            // disabled={loading}
             className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
           >
             {loading ? 'Loading...' : 'Sign In'}
           </button>
+
           <OAuth />
         </form>
         <div className='flex gap-2 mt-5'>
