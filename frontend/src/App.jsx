8c21cbc46b463c36e752c030b2f045a398f5650e
Lithira Amarasinghe <amarasinghelithira@gmail.com>
2025-01-05 00:35:10 +0530

    QR scanner integrated.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import PrivateRoute1 from './Components/PrivateRouteProfile';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Gallery from './Pages/Gallery';
 import ARegistrations from './Pages/AdminPages/ARegistrations';
+import PassActivity from './Pages/Activity/PassActivity';
 
 function App() {
   return (
@@ -23,7 +24,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-         
+          <Route path="/pass-activity" element={<PassActivity />} />
           <Route path="/about" element={<About />} />
           <Route element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
@@ -38,6 +39,7 @@ function App() {
           <Route element={<PrivateRoute1 />}>
             <Route path="/sprofiles" element={<Sprofiles />} />
           </Route>
+          
           
           
         </Routes>
