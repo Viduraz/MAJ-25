2ef4589d05faa1c7b1d181dc8b00c45fb953acdb
Viduraz <162951228+Viduraz@users.noreply.github.com>
2025-01-12 14:04:10 +0530

    Admin activity pages created with crud and qr done

--- backend/controllers/registration.controller.js
+++ backend/controllers/registration.controller.js
@@ -39,7 +39,8 @@ export const getUserByEmail = async (req, res) => {
         res.status(200).json(registration);
     } catch (error) {
         res.status(404).json({ message: error.message });
-      
+    }     
+};
 export const getAllRegistration = async (req, res) => {
     try {
         const registration = await Registration.find();
@@ -47,6 +48,7 @@ export const getAllRegistration = async (req, res) => {
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
+
 };
 
 export const updateRegistration = async (req, res) => {
