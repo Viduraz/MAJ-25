01c7c1e9774c8e29a06deb1ba6f2c93198f250b9
Viduraz <162951228+Viduraz@users.noreply.github.com>
2025-02-01 01:47:19 +0530

    errorjs addded

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import User from '../models/user.model.js';
-import { errorHandler } from '../utils/error.js';
+import { errorHandler } from '../Utils/error.js';
 import bcryptjs from 'bcryptjs';
 
 
