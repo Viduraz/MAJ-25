30e114472114100097593ef0971d2b01e8900039
Viduraz <162951228+Viduraz@users.noreply.github.com>
2025-02-03 14:11:54 +0530

    sanjula

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -3,7 +3,7 @@ import AnimationContainer from "@/Components/AnimationContainer";
 import aboutus from "../Assests/Aboutus.jpg";
 import yasas from "../Assests/Yasas1.jpg";
 import savindu from "../Assests/Savindu1.jpg";
-import sanjula from "../Assests/Sanjula.jpg";
+import sanjula from "../Assests/Sanjula1.jpg";
 import wohansa from "../Assests/Wohansa.jpg";
 import pahanmi from "../Assests/Pahanmi.jpg";
 import niketha from "../Assests/Niketha.jpg";
