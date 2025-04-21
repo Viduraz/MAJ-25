3041d93968cb8ea86c75501e7e03a84e505c6578
viduraz <viduraruwantha2002@gmail.com>
2025-04-21 23:23:17 +0530

    d changes

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,10 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+    res.send('🎉 Backend is working!');
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
